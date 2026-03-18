8afd11f490f15a5d8338fc97385420549b08d3b2
Mayphus <tangmeifa@gmail.com>
2026-03-18 12:50:12 +0800

    Chore: Update comments in xhs-2699182486 customer profile

--- yuanshu-profiles/xhs-2699182486.mk
+++ yuanshu-profiles/xhs-2699182486.mk
@@ -1,14 +1,14 @@
-# tony-yuanshu.mk
+# xhs-2699182486.mk
 
-# Tony only needs the shuffle17 layout
+# Customer only needs the shuffle17 layout
 YUANSHU_SCHEMAS := shuffle17_ice
 
-# Tony's patches
+# Customer's patches
 YUANSHU_CUSTOM_PATCHES := shuffle17_ice
 
-# Tony's skins
+# Customer's skins
 PROFILE_SKINS := shuffle17
 
 # Note: The Makefile will automatically pull the 'rime_ice' 
-# dictionaries and 'cangjie6' reverse-lookup dictionaries 
-# because it detects shuffle17_ice in the schema list!
+# dictionaries and folders because it detects shuffle17_ice 
+# in the schema list.
